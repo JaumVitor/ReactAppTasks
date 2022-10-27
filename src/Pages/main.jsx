4d32f3a9c75a7313210bd2090db8cda8ad87b83c
Jaum Vitor <joao.vitor.pires.060@gmail.com>
2022-10-27 19:00:08 -0300

    Config router dom in main

--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '../Components/App/App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { DetailsTask } from '../Components/DetailsTask/DetailsTask'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<App />}>
+        </Route>
+        <Route
+          path='/details'
+          element={<DetailsTask/>}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
